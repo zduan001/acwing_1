eddcfc33bf4cb81f495c8957a1ee29c04e77b017
David Duan <davidzduan@outlook.com>
2019-12-31 06:24:10 +0000

    employee free time lc 759

---
+++ ab/employee_free_time_lc_759.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define x first
+#define y second
+
+typedef pair<int, int> pii;
+
+class Solution {
+public:
+    vector<pii> employeeFreeTime(vector<vector<pii>>& schedule) {
+      vector<pii> ends;
+      for (auto items : schedule) {
+        for (auto item : items) {
+          ends.push_back({item.x, 1});
+          ends.push_back({item.y, -1});
+        }
+      }
+
+      sort(ends.begin(), ends.end());
+
+      int l = -1;
+      int count = 0;
+      vector<pii> res;
+      for (auto item : ends) {
+        count += item.y;
+        if (!count) l = item.first;
+        if (count && l > 0) {
+          if (l > 0) {
+            if (l != item.x) res.push_back({l, item.x});
+            l = -1;
+          }
+        }
+      }
+      return res;
+    }
+};
+
+int main() {
+  Solution sol;
+  vector<vector<pii>> input = {{{1,3},{5,7}},{{2,4}},{{2,5},{9,12}}};
+  auto res = sol.employeeFreeTime(input);
+  for (auto item : res) {
+    cout << item.x << " " << item.y << endl;
+  }
+  return 0;
+}
