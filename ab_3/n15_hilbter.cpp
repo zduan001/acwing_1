e2bcf03ae43c3dfb471fc7d2b9b084f5a2e93a63
david <david@davidserver.com>
2020-01-15 06:30:57 +0000

    n15

---
+++ ab_3/n15_hilbter.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int xy2d(int tier, int x, int y) {
+  if (tier == 0) return 1;
+  int len = 1 << (tier-1);
+  int num = 1 << (2*(tier-1));
+
+  if (x < len && y >= len) {
+    return num + xy2d(tier-1, x, y-len);
+  } else if (x >= len && y >= len) {
+    return 2 * num + xy2d(tier-1, x - len, y - len); 
+  } else if (x < len && y < len) {
+    return xy2d(tier-1, y, x);
+  } else {
+    return 3 * num + xy2d(tier -1, len-1-y, 2*len-1 -x); // remember 2*len-1-y, len-1-x;
+  }
+}
+
+int main() {
+    cout<< xy2d(2, 3, 1) << endl;
+    cout<< xy2d(2, 2, 1) << endl;
+    cout<< xy2d(2, 2, 0) << endl;
+    cout<< xy2d(2, 3, 0) << endl;
+    cout<< xy2d(2, 2, 2) << endl;
+    cout<< xy2d(2, 0, 2) << endl;
+    return 0;
+}
+// expect result.
+// 13
+// 14
+// 15
+// 16
+// 9
+// 5
