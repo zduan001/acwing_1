0b212b0600bdb96ff734ebae46130912de073cfe
david <david@davidserver.com>
2020-01-16 03:04:40 +0000

    n18

--- ab_3/n18_round_price.cpp
+++ ab_3/n18_round_price.cpp
@@ -12,21 +12,20 @@ public:
         vector<double> diff;
         for (string x : prices) {
             double val = stod(x);
-            int tmp = (int) val;
-            sum += (int)tmp;
-            if (val == (double)tmp *1.0) continue;
-            diff.push_back(val - 1.0 * tmp);
+            sum += (int)val;
+            if (val == (int)val * 1.0) continue;
+            diff.push_back(val - 1.0 * (int)val);
             n++;
         }
 
         if (target < sum || target > (sum+n)) return "-1";
-        int nfloor = n - (target -sum);
+        int nfloor = n - (target - sum);
         nth_element(diff.begin(), diff.begin()+nfloor, diff.end());
 
         double res = 0.0;
         for (int i = 0; i < diff.size();i++) {
-            if (i < n) res += diff[i];
-            else res += 1.0 - diff[i];
+            if (i < nfloor) res += diff[i];
+            else res += (1.0 - diff[i]);
         }
         
         string s = to_string(res);
@@ -36,5 +35,9 @@ public:
 };
 
 int main() {
+    vector<string> input = {"1.2","2.3","3.4"};
+    Solution sol;
+    cout << sol.minimizeError(input, 7) << endl; // expect 1.100
+    return 0;
 
 }
